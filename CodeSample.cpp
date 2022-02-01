d2830b4b5b2652abfbe642c90e4813f733463f9d
Mariam-ElG <98828352+Mariam-ElG@users.noreply.github.com>
2022-02-01 20:11:23 +0200

    Update CodeSample.cpp

--- CodeSample.cpp
+++ CodeSample.cpp
@@ -1,1 +1,13 @@
-#include <iostream>using namespace std;int main(){    cout<< "Hello, this is a demo to test Git and Github \n";    int id;    cin >> id;	return 0;}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<< "Hello, this is a demo to test Git and Github \n";
+    int id;
+    cout<<"Enter your ID: "<<endl;
+    cin >> id;
+	return 0;
+}
+
+
